c68d5c31a8cef4a68bea57713d33514d1db7b90f
Marty Bonacci <martybonacci@gmail.com>
2017-07-12 13:50:13 -0600

    created table

--- index.php
+++ index.php
@@ -2,9 +2,28 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>qHTML Sandbox</title>
+		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>Hello, World! :D</h1>
+		<h1>This is my web page</h1>
+
+		<table>
+			<tr>
+				<th>cats</th>
+				<th>dogs</th>
+			</tr>
+			<tr>
+				<td>persian</td>
+				<td>beagle</td>
+			</tr>
+			<tr>
+				<td>maine coon</td>
+				<td>rottweiler</td>
+			</tr>
+			<tr>
+				<td>siamese</td>
+				<td>german shepard</td>
+			</tr>
+		</table>
 	</body>
 </html>
